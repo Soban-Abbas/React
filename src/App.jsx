8691cf82cebc5613916bc120c944b7ab451d6e07
Soban Abbas <sobanabbass@gmail.com>
2026-05-22 11:59:50 +0500

    rendering list dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,11 @@ function App() {
         <h2>Time to get started!</h2>
         <section id='core-concepts'>
           <ul>
-            <CoreConpts title={Core_Concepts_Data[0].title}
-              image={Core_Concepts_Data[0].image}
-              discription={Core_Concepts_Data[0].discription} />
-
-
-            <CoreConpts title={Core_Concepts_Data[1].title} discription={Core_Concepts_Data[1].discription}
-              image={Core_Concepts_Data[1].image} />
-            <CoreConpts title={Core_Concepts_Data[2].title} discription={Core_Concepts_Data[2].discription} image={Core_Concepts_Data[2].image} />
-            <CoreConpts title={Core_Concepts_Data[3].title} image={Core_Concepts_Data[3].image} discription={Core_Concepts_Data[3].discription} />
+           
+            {Core_Concepts_Data.map((concept)=>{
+             return <CoreConpts key={concept.title} title={concept.title} discription={concept.discription}
+                image={concept.image} />
+            })}
           </ul>
         </section>
         <section id='examples'>
